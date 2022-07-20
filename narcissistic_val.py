6c6131c4213e858e5f48d64993a921abfb89c05f
Tyler Sather <91901337+TyLrrrr@users.noreply.github.com>
2022-07-20 12:42:30 -0500

    Create narcissistic_val.py

---
+++ narcissistic_val.py
@@ -0,0 +1,12 @@
+def narcissistic( value ):
+    # A Narcissistic Number is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base.
+    # Your code must return true or false depending upon whether the given number is a Narcissistic number in base 10.
+    x = len(str(value))
+    y = 0
+    z = str(value)
+    for i in z:
+        y += int(i)**x
+    if y == value:
+        return True
+    else:
+        return False
