865cfd0129c0741719e1b1cb80a641751c72bcbc
Tyler Sather <91901337+TyLrrrr@users.noreply.github.com>
2022-07-20 12:46:56 -0500

    Update Alpha_to_int.py

--- Alpha_to_int.py
+++ Alpha_to_int.py
@@ -1,3 +1,5 @@
+# Codewar Challenge: In this kata you are required to, given a string, replace every letter with its position in the alphabet. a=1, b=2, etc...
+#ignore non alpha chars
 def alphabet_position(text):
     x = "".join(text).lower().strip(".").replace(" ","")
     y = ""
