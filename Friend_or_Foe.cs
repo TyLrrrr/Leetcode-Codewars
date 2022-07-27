832e0fd0eae8c168887ac9b3899bf1f4acecd3fa
Tyler Sather <91901337+TyLrrrr@users.noreply.github.com>
2022-07-27 11:21:56 -0500

    Create Friend_or_Foe.cs

---
+++ Friend_or_Foe.cs
@@ -0,0 +1,21 @@
+// Codewars Challenge: Make a program that filters a list of strings and returns a list with only your friends name in it.
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+using System;
+using System.Collections.Generic;
+
+public static class Kata {
+  public static IEnumerable<string> FriendOrFoe(string[] names) 
+  {
+    List<string> friend = new List<string>();
+    foreach(var i in names)
+    {
+      if(i.Length == 4)
+      {
+        friend.Add(i);
+      }
+    } 
+    return friend;
+  }
+}
